7682edcb27e23d5aff9192758a3bad85d9963321
Vojimir Raovic <voj@norwoodsystems.com>
2021-03-04 15:12:20 +0800

    Revert "Added nullability specifiers."
    This reverts commit c78e55148621fa0b8348cf2c5935e1cb9c12b49d.

--- peertalk/PTChannel.h
+++ peertalk/PTChannel.h
@@ -10,8 +10,6 @@
 #import "PTProtocol.h"
 #import "PTUSBHub.h"
 
-NS_ASSUME_NONNULL_BEGIN
-
 @class PTData, PTAddress;
 @protocol PTChannelDelegate;
 
@@ -51,29 +49,29 @@ NS_ASSUME_NONNULL_BEGIN
 
 
 // Connect to a TCP port on a device connected over USB
-- (void)connectToPort:(int)port overUSBHub:(PTUSBHub*)usbHub deviceID:(NSNumber*)deviceID callback:(void(^ _Nullable)(NSError * _Nullable error))callback;
+- (void)connectToPort:(int)port overUSBHub:(PTUSBHub*)usbHub deviceID:(NSNumber*)deviceID callback:(void(^)(NSError *error))callback;
 
 // Connect to a TCP port at IPv4 address. Provided port must NOT be in network
 // byte order. Provided in_addr_t must NOT be in network byte order. A value returned
 // from inet_aton() will be in network byte order. You can use a value of inet_aton()
 // as the address parameter here, but you must flip the byte order before passing the
 // in_addr_t to this function.
-- (void)connectToPort:(in_port_t)port IPv4Address:(in_addr_t)address callback:(void(^ _Nullable)(NSError * _Nullable error, PTAddress *address))callback;
+- (void)connectToPort:(in_port_t)port IPv4Address:(in_addr_t)address callback:(void(^)(NSError *error, PTAddress *address))callback;
 
 // Listen for connections on port and address, effectively starting a socket
 // server. Provided port must NOT be in network byte order. Provided in_addr_t
 // must NOT be in network byte order.
 // For this to make sense, you should provide a onAccept block handler
 // or a delegate implementing ioFrameChannel:didAcceptConnection:.
-- (void)listenOnPort:(in_port_t)port IPv4Address:(in_addr_t)address callback:(void(^ _Nullable)(NSError * _Nullable error))callback;
+- (void)listenOnPort:(in_port_t)port IPv4Address:(in_addr_t)address callback:(void(^)(NSError *error))callback;
 
 // Send a frame with an optional payload and optional callback.
 // If *callback* is not NULL, the block is invoked when either an error occured
 // or when the frame (and payload, if any) has been completely sent.
-- (void)sendFrameOfType:(uint32_t)frameType tag:(uint32_t)tag withPayload:(dispatch_data_t _Nullable)payload callback:(void(^ _Nullable)(NSError * _Nullable error))callback;
+- (void)sendFrameOfType:(uint32_t)frameType tag:(uint32_t)tag withPayload:(dispatch_data_t)payload callback:(void(^)(NSError *error))callback;
 
 // Lower-level method to assign a connected dispatch IO channel to this channel
-- (BOOL)startReadingFromConnectedChannel:(dispatch_io_t)channel error:(__autoreleasing NSError** _Nullable)error;
+- (BOOL)startReadingFromConnectedChannel:(dispatch_io_t)channel error:(__autoreleasing NSError**)error;
 
 // Close the channel, preventing further reading and writing. Any ongoing and
 // queued reads and writes will be aborted.
@@ -110,21 +108,19 @@ NS_ASSUME_NONNULL_BEGIN
 
 @required
 // Invoked when a new frame has arrived on a channel.
-- (void)ioFrameChannel:(PTChannel* _Nonnull)channel didReceiveFrameOfType:(uint32_t)type tag:(uint32_t)tag payload:(PTData* _Nullable)payload;
+- (void)ioFrameChannel:(PTChannel*)channel didReceiveFrameOfType:(uint32_t)type tag:(uint32_t)tag payload:(PTData*)payload;
 
 @optional
 // Invoked to accept an incoming frame on a channel. Reply NO ignore the
 // incoming frame. If not implemented by the delegate, all frames are accepted.
-- (BOOL)ioFrameChannel:(PTChannel* _Nonnull)channel shouldAcceptFrameOfType:(uint32_t)type tag:(uint32_t)tag payloadSize:(uint32_t)payloadSize;
+- (BOOL)ioFrameChannel:(PTChannel*)channel shouldAcceptFrameOfType:(uint32_t)type tag:(uint32_t)tag payloadSize:(uint32_t)payloadSize;
 
 // Invoked when the channel closed. If it closed because of an error, *error* is
 // a non-nil NSError object.
-- (void)ioFrameChannel:(PTChannel* _Nonnull)channel didEndWithError:(NSError* _Nullable)error;
+- (void)ioFrameChannel:(PTChannel*)channel didEndWithError:(NSError*)error;
 
 // For listening channels, this method is invoked when a new connection has been
 // accepted.
-- (void)ioFrameChannel:(PTChannel* _Nonnull)channel didAcceptConnection:(PTChannel* _Nonnull)otherChannel fromAddress:(PTAddress* _Nonnull)address;
+- (void)ioFrameChannel:(PTChannel*)channel didAcceptConnection:(PTChannel*)otherChannel fromAddress:(PTAddress*)address;
 
 @end
-
-NS_ASSUME_NONNULL_END
